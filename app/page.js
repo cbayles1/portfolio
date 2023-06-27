7289785770de1d04138b8347b52f9ffee562b5e4
Caleb <clbayles@hotmail.com>
2023-06-26 21:05:37 -0500

    tweaks on main page, fixed portrait

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="bg-oat w-full h-full p-5 font-medium">
     
-    <span id="contact" className='flex top-2 justify-end sticky m-0'>
+    <span id="contact" className='flex top-2 justify-end sticky m-0 z-10'>
       <ContactImg name="Email" path="mailto:clbayles@hotmail.com" image="/envelope.svg"></ContactImg>
       <ContactImg name="LinkedIn" path="https:/www.linkedin.com/in/caleb-bayles/" image="/linkedin.svg"></ContactImg>
       <ContactImg name="Github" path="https:/www.github.com/cbayles1" image="/github.svg"></ContactImg>
@@ -21,15 +21,17 @@ export default function Home() {
         <p id="bio" className="max-w-sm">BIO Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
 
-      <div className="w-[150px] h-[200px] lg:w-[150px] lg:h-[200px] relative">
-        <Image src="/portrait.jpg" alt="photo of me" fill id='portrait'></Image>
-      </div>
+      <button className="w-[300px] h-[400px] relative m-2 overflow-hidden rounded-xl border-2">
+        <span>
+        <Image src="/portrait.jpg" alt="photo of me" fill className='absolute object-cover'></Image>
+        </span>
+      </button>
 
     </div>
 
     <br></br>
 
-    <div id='categories' className="my-1.5 mx-10 bg-alpine grid text-center p-3 grid-cols-1 lg:grid-cols-3 text-oat">    
+    <div id='categories' className="my-1.5 mx-10 bg-alpine grid text-center p-3 grid-cols-1 lg:grid-cols-3 text-oat rounded-lg">    
       
         <div id='skills' className='h-auto m-2 p-1'>
           <Link href="/skills">
