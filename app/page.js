16032ed4058ac0eb2ee870f13fb1dfc62d45b464
Caleb <clbayles@hotmail.com>
2023-07-01 16:55:58 -0500

    bio margin changes when large screen

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
   <main className="bg-oat w-full h-full p-5 font-medium">
 
-    <div className='flex justify-center flex-wrap'>
+    <div className='flex justify-center flex-wrap lg:ml-32'>
       <p id="bio" className="max-w-sm m-6">
         BIO Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
