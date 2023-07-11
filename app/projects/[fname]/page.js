f1c91f5115ea620bc854ab152ab315ff45ac9a3c
Caleb <clbayles@hotmail.com>
2023-07-10 19:32:13 -0500

    solved routing and data fetching issues

--- app/projects/[fname]/page.js
+++ app/projects/[fname]/page.js
@@ -2,73 +2,37 @@ import Image from 'next/image';
 import Link from 'next/link';
 import allProjects from '../../projectData.json';
 
-/*function getAllProjectFilenames() {
-    let fnames = [];
-    for (const entry of allProjects) {
-        fnames.push(entry.fname);
-    }
-
-    return fnames;
-}*/
-
-function getProjectData(projectName) {
+async function getProjectData(projectName) {
     let projectData = null;
     for (const entry of allProjects) {
         if (entry.fname === projectName) {
             projectData = entry;
+            break;
         }
     }
     return projectData;
 }
 
-export async function getStaticPaths() {
-    const paths = allProjects.map(project => {
-        return {
-            params: {
-                fname: project.fname
-            }
-        }
-    });
-
-    return {
-        paths: paths,
-        fallback: false
+export async function generateStaticParams() { // It would appear this is unneccesary for now, 
+    let answers = [];                          // but it doesn't hurt anything so I'll leave it alone.
+    for (const entry of allProjects) {
+        answers.push({fname: entry.fname});
     }
+    return answers;
 }
 
-export async function getStaticProps(context) {
-    
-    const {params} = context;
+export default async function Page({params: {fname}}) {
 
-    //let data = getProjectData(params.fname);
-    const data = {
-        title: "Expendable 054",
-        fname: "expendable054",
-        desc: "hi there this the description",
-        img: "here image path",
-        github: "here github url",
-        link: "here other link if available"
-    }
-
-    return {
-        props: {
-            projectData: data
-        }
-    }
-}
-
-export default function Home(context) {
-
-    const projectData = context.params;
+    let projectData = await getProjectData(fname);
 
     return (
-    <div className='grid grid-cols-6 m-1'>    
+    <div className='grid grid-cols-6 m-1'>
         <ul className='m-4'>
-            <li>Title: {projectData.title}</li>
-            <li>Desc: {projectData.desc}</li>
-            <li>Img: {projectData.img}</li>
-            <li>GitHub: {projectData.github}</li>
-            <li>Link: {projectData.link}</li>
+            <li>Title: {projectData?.title}</li> {/* ? means the data is only displayed if it exists */}
+            <li>Desc: {projectData?.desc}</li>
+            <li>Img: {projectData?.img}</li>
+            <li>GitHub: {projectData?.github}</li>
+            <li>Link: {projectData?.link}</li>
         </ul>
     </div>
     );
