3dddbe89043336770da49ee2262f998b812b7ede
Caleb <clbayles@hotmail.com>
2023-07-20 19:16:17 -0500

    test using netflix external link

--- app/layout.js
+++ app/layout.js
@@ -39,6 +39,10 @@ export default function RootLayout({ children }) {
 
         {children}
 
+        
+        <a rel="noopener noreferrer" role="link" href="http://www.netflix.com/">netflix.com</a>
+
+
         <div id='footer' className="mx-3 my-1.5 py-1.5 text-center font-light text-nero mb-4">
           <p id="contact" className='mb-1'>
             <BubbleLink name="clbayles@hotmail.com" href="mailto:clbayles@hotmail.com"></BubbleLink>
